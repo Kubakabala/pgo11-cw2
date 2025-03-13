1485d03af5b2cb309559dc5a4bf8edfbc81252df
Kubakabala <s32301@pjwstk.edu.pl>
2025-03-13 11:07:19 +0100

    zad 2

---
+++ zad2.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
+public class zad2 {
+    public static int findDominant(int[] array) {
+        int n = array.length;
+        int threshold = n / 2;
+        Map<Integer, Integer> countMap = new HashMap<>();
+        for (int num : array) {
+            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
+            if (countMap.get(num) > threshold) {
+                return num;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] test1 = {3, 3, 3, 2, 2};
+        int[] test2 = {1, 2, 3, 4};
+        int[] test3 = {5, 5, 5, 1};
+        System.out.println(findDominant(test1));
+        System.out.println(findDominant(test2));
+        System.out.println(findDominant(test3));
+    }
+}
